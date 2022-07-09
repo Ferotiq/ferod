53e22eb7be144210e807ccab4f2cd3e0592c4bdf
Evan <evanrayweigel@outlook.com>
2022-07-09 12:12:14 -0500

    reorder

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,5 +1,5 @@
 export { Client } from "./structures/Client";
 export { Command } from "./structures/Command";
 export { Event } from "./structures/Event";
-export * from "./types";
 export { toPascalCase } from "./util/toPascalCase";
+export * from "./types";
