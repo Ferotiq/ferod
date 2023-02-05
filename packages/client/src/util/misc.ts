3695f69ea632e01e68459728fbcc3cb78a685229
Ferotiq <evanrayweigel@outlook.com>
2023-02-04 19:52:44 -0600

    remove unused function

--- packages/client/src/util/misc.ts
+++ packages/client/src/util/misc.ts
@@ -1,5 +1,4 @@
 import globCallback from "glob";
-import _ from "lodash";
 import path from "path";
 import { pathToFileURL } from "url";
 import { promisify } from "util";
@@ -14,19 +13,6 @@ export function quickClean<T>(obj: T): T {
   return JSON.parse(JSON.stringify(obj));
 }
 
-/**
- * Removes all deeply nested nil properties from an object.
- * https://stackoverflow.com/a/48584441
- * @param obj The object to clean
- */
-export function clean(obj: any): any {
-  return _(obj)
-    .pickBy(_.isObject)
-    .mapValues(clean)
-    .assign(_.omitBy(obj, _.isObject))
-    .omitBy(_.isNil)
-    .value();
-}
 /**
  * Imports multiple files and returns them as instances of the class specified.
  * @param filePath The path to import
