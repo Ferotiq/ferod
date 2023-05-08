8d18d3c423c0a79097111594f1f0ee9bae81873b
Evan <evanrayweigel@outlook.com>
2023-05-07 20:30:16 -0500

    make cli.js the bin entry point

--- packages/cli/bin/ferod.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node --experimental-modules --no-warnings
-
-import { cli } from "../dist/cli.js";
-
-cli(process.argv);
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node --experimental-specifier-resolution=node --experimental-modules --no-warnings
 import { Command } from "commander";
 import { version } from "../package.json";
 import { createFerodApp } from "./commands/create-app";
@@ -10,78 +11,70 @@ import type {
 	CreateEventListenerOptions
 } from "./types";
 
-/**
- * Basic CLI
- * @param args The arguments to parse.
- */
-export function cli(args: string[]): void {
-	const program = new Command("ferod");
+const program = new Command("ferod")
+	.description("Create a new Ferod app/command/event.")
+	.argument("[create|test]", "The command to run", "create")
+	.argument(
+		"[app|command|event-listener]",
+		"The type of Ferod app/command/event to create.",
+		"app"
+	)
+	.argument("[name]", "The name of the Ferod app/command/event to create.")
+	.option("--no-install, --noInstall", "Do not install dependencies.")
+	.option("--no-git, --noGit", "Do not initialize a git repository.")
+	.option("-y, --yes", "Answer yes to all questions.")
+	.version(version, "-v, --version", "Show the version.")
+	.parse(process.argv);
 
-	program
-		.description("Create a new Ferod app/command/event.")
-		.argument("[create|test]", "The command to run", "create")
-		.argument(
-			"[app|command|event-listener]",
-			"The type of Ferod app/command/event to create.",
-			"app"
-		)
-		.argument("[name]", "The name of the Ferod app/command/event to create.")
-		.option("--no-install, --noInstall", "Do not install dependencies.")
-		.option("--no-git, --noGit", "Do not initialize a git repository.")
-		.option("-y, --yes", "Answer yes to all questions.")
-		.version(version, "-v, --version", "Show the version.")
-		.parse(args);
+const [command, subcommand, name] = program.args;
 
-	const [command, subcommand, name] = program.args;
+switch (command ?? "create") {
+	case "create": {
+		switch (subcommand ?? "app") {
+			case "app": {
+				const options: CreateAppOptions = {
+					name,
+					flags: program.opts<CLIFlags>()
+				};
 
-	switch (command ?? "create") {
-		case "create": {
-			switch (subcommand ?? "app") {
-				case "app": {
-					const options: CreateAppOptions = {
-						name,
-						flags: program.opts<CLIFlags>()
-					};
-
-					createFerodApp(options);
-					break;
-				}
-
-				case "command": {
-					const options: CreateCommandOptions = {
-						name
-					};
-
-					createFerodCommand(options);
-
-					break;
-				}
-
-				case "event-listener":
-				case "listener":
-				case "event": {
-					const options: CreateEventListenerOptions = {
-						name
-					};
-
-					createFerodEventListener(options);
-
-					break;
-				}
-
-				default:
-					program.help();
+				createFerodApp(options);
+				break;
 			}
 
-			break;
+			case "command": {
+				const options: CreateCommandOptions = {
+					name
+				};
+
+				createFerodCommand(options);
+
+				break;
+			}
+
+			case "event-listener":
+			case "listener":
+			case "event": {
+				const options: CreateEventListenerOptions = {
+					name
+				};
+
+				createFerodEventListener(options);
+
+				break;
+			}
+
+			default:
+				program.help();
 		}
 
-		case "test":
-			console.log("Test command");
-
-			break;
-
-		default:
-			program.help();
+		break;
 	}
+
+	case "test":
+		console.log("Test command");
+
+		break;
+
+	default:
+		program.help();
 }
